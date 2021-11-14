9ee4ca3ad593d4fac9aac8bbf30a8179a9b91079
sayalikeche <kechesayali11@gmail.com>
2021-11-14 20:54:34 +0530

    [sayali Added Attendance]

--- EmplooyeWageComp/EmplooyeWageComp/Program.cs
+++ EmplooyeWageComp/EmplooyeWageComp/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 
-namespace EmplooyeWageComp
+namespace EmployeeAttendance
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employe Wage Computation");
-            Console.ReadLine();
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == IS_FULL_TIME)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
         }
     }
 }
