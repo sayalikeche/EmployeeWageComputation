075637f42283af7e3de28028e92a2f1bec2f94eb
sayalikeche <kechesayali11@gmail.com>
2021-11-14 20:50:06 +0530

    [sayali Adding Employee Wage Computation]

---
+++ EmplooyeWageComp/EmplooyeWageComp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmplooyeWageComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employe Wage Computation");
+            Console.ReadLine();
+        }
+    }
+}
